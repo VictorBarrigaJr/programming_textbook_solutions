25da1ac1f0bb6122118e6da383feeb06f9d0fe0a
Victor Barriga <victorbarriga@live.com>
2014-10-13 17:00:31 -0700

    added program11.8.c

---
+++ programming_c/chapter11/program11.8.c
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Program 11.8 - Using Pointers and Functions - from Programming in C
+ * File:    program11.8.c
+ * 
+ * Victor Barriga 
+ * victorbarriga@live.com
+ * 
+ * Program to illustrate using pointers and functions
+ *
+ *******************************************************************************/
+
+#include <stdio.h>
+
+void test (int *int_pointer);
+
+int main (void)
+{
+    void test (int *int_pointer);
+    int i = 50, *p = &i;
+
+    printf ("Before the call to test i = %i\n", i);
+    
+    test (p);
+    printf ("After the call to test i = %i\n", i);
+
+    return 0;
+}
+
+void test (int *int_pointer)
+{
+    *int_pointer = 100;
+}
